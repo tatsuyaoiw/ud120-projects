8451a2ce449cc6ad6687bb9c09a405e189ad9904
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-09-02 15:09:23 +0900

    Answer to Support Vector Classification mini project problem

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# from sklearn.svm import SVC
+from sklearn.svm import SVC
+clf = SVC(kernel='linear')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+accuracy = clf.score(features_test, labels_test)
+print accuracy
+
 #########################################################
 
 
