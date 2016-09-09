f62938ae5432348638fbadd037e175dd2fe0cf37
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2016-09-09 10:03:11 +0900

    Add predict and time to SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -33,6 +33,10 @@ t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
 accuracy = clf.score(features_test, labels_test)
 print accuracy
 
